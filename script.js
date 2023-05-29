1a6563e11c3ed909a4de8f2f46c3be31265d2436
Dzikri Raihan <dziiikriii@gmail.com>
2023-05-29 21:59:35 +0700

    beta

---
+++ script.js
@@ -0,0 +1,11 @@
+function alertShare() {
+  alert("Link sudah dicopy!");
+}
+
+function alertPembayaran() {
+  alert("Pembayaran telah berhasil!");
+}
+
+function alertProfil() {
+  alert("Profil berhasil diubah!");
+}
