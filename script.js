24b739ce595bb2a923cedfe3b966d4dd9c2806a8
Dzikri Raihan <dziiikriii@gmail.com>
2023-06-16 14:02:44 +0700

    responsif

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function alertPembayaran() {
 function alertProfil() {
   alert("Profil berhasil diubah!");
 }
+
+//toggle class active
+// const navbarNav = document.querySelector(".navbar-nav");
+
+// // ketika hamburger menu diklik
+// document.querySelector(".navbar-toggler").onclick = () => {
+//   navbarNav.classList.toggle("active");
+// };
+
+// //klik di luar sidebar untuk menghilangkan nav
+
+// const hamburger = document.querySelector(".navbar-toggler");
+// document.addEventListener("click", function (e) {
+//   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove("active");
+//   }
+// });
